dba114e5ca75c1c0e1f97d3ac1f2941966bf27a1
marolive <marolive@redhat.com>
2025-05-01 18:58:07 -0300

    Add SQLAlchemy - criacao de banco de dados

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Configurações da Aplicação
 # class Config:
@@ -19,7 +20,25 @@ app = Flask(__name__)
 
 app.config.from_object('config')
 
-print(app.config['SECRET_KEY'])
-print(app.config['SQLALCHEMY_DATABASE_URI'])
-print(app.config['SWAGGER'])
-print(app.config['CACHE_TYPE'])
+# print(app.config['SECRET_KEY'])
+# print(app.config['SQLALCHEMY_DATABASE_URI'])
+# print(app.config['SWAGGER'])
+# print(app.config['CACHE_TYPE'])
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    time_minutes = db.Column(db.Integer, nullable=False)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("Banco de dados criado!")
